ee382af6de67a39efb165c5937046b42c03e0318
Aruna Turay <tura_arun@yahoo.com>
2023-04-11 16:00:11 -0400

    listener

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function addingEventListener() {
   const input = document.getElementById("button");
   input.addEventListener("click", function() {
-    alert("I was clicked");
+    alert("I was clicked twice");
   })
 }
